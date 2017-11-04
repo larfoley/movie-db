96aa53f441718bd951c4cea2c811ff9380fddf22
larfoley <lar_x87@yahoo.com>
2017-11-04 17:58:36 +0000

    added ejs client side functionality

---
+++ client/js/ejsClient.js
@@ -0,0 +1,28 @@
+var ejs = require("./vendor/ejs.js");
+
+module.exports = (function() {
+
+  var path = "/templates/"
+
+  return {
+
+    setTemplatesLocation: function(path) {
+      path = path;
+    },
+
+    render: function(template, data, callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", path + template + ".ejs", true);
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4) {
+          if (this.status >= 200 && this.status < 400) {
+            callback(ejs.render(this.responseText, data))
+          }
+        }
+      }
+      xhr.send();
+    }
+
+  }
+
+}())
