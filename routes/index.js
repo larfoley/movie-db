674ae0aaee2226788625bf0f56db7d150db2be76
larfoley <lar_x87@yahoo.com>
2017-11-17 11:22:02 +0000

    retrieved content from api for home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
+var config = require('../config');
 var express = require('express');
 var router = express.Router();
 var https = require('https');
+var request = require("request");
+var rp = require('request-promise');
+
+var options = { method: 'GET',
+  url: 'https://api.themoviedb.org/3/discover/movie',
+  qs: {
+    page: '1',
+    sort_by: 'popularity.desc',
+    language: 'en-US',
+    api_key: config.api_key
+  },
+  body: '{}' };
+
+var options2 = { method: 'GET',
+  url: 'https://api.themoviedb.org/3/discover/movie',
+  qs:
+   { page: '1',
+     include_video: 'false',
+     include_adult: 'false',
+     sort_by: 'primary_release_date.desc',
+     language: 'en-US',
+     api_key: config.api_key },
+  body: '{}' };
+
+
 
 router.get('/', function(req, res, next) {
-  res.render('pages/index', {activeLink: "home"});
+
+  var popularMovies, latestMovies;
+
+  rp(options)
+    .then(function (response) {
+      // Get popularMovies
+      popularMovies = JSON.parse(response).results;
+      return rp(options2)
+    })
+    .then(function (response) {
+      // Get latestMovies
+      latestMovies = JSON.parse(response).results;
+      res.render('pages/index', {
+        activeLink: "home",
+        popularMovies: popularMovies,
+        latestMovies: latestMovies
+      });
+    })
+    .catch(function (err) {
+      next(err)
+    });
 });
 
 module.exports = router;
