2e4817a31186824c6423a91b52ce43c3ca775f5e
Eoghan <eoghankenny95@gmail.com>
2017-11-21 18:28:54 +0000

    passed media-type to media.ejs

--- routes/media.js
+++ routes/media.js
@@ -6,49 +6,50 @@ var request = require("request");
 var rp = require('request-promise');
 
 var options2 = {
-	method: 'GET',
-	url: 'https://api.themoviedb.org/3/discover/movie',
-	qs: {
-		page: '1',
-		include_video: 'false',
-		include_adult: 'false',
-		sort_by: 'primary_release_date.desc',
-		language: 'en-US',
-		api_key: config.api_key
-	},
-	body: '{}'
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/discover/movie',
+  qs: {
+    page: '1',
+    include_video: 'false',
+    include_adult: 'false',
+    sort_by: 'primary_release_date.desc',
+    language: 'en-US',
+    api_key: config.api_key
+  },
+  body: '{}'
 };
 
 router.get('/', function (req, res, next) {
-	var id = req.query.id;
-	var selectedMovie, relatedMovies;
+  var id = req.query.id;
+  var mediaType = req.query.media_type;
+  var media, relatedMovies;
 
-	rp({
-			method: 'GET',
-			url: 'https://api.themoviedb.org/3/movie/' + id,
-			qs: {
-				api_key: config.api_key
-			},
-			body: '{}'
-		})
-		.then(function (response) {
-			// Get selectedMovie
-			selectedMovie = JSON.parse(response);
-			console.log(JSON.parse(response));
-			return rp(options2)
-		})
-		.then(function (response) {
-			// Get latestMovies
-			relatedMovies = JSON.parse(response).results;
-			res.render('pages/media', {
-				activeLink: "home",
-				selectedMovie: media,
-				relatedMovies: relatedMovies
-			});
-		})
-		.catch(function (err) {
-			next(err)
-		});
+  rp({
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/movie/' + id,
+      qs: {
+        api_key: config.api_key
+      },
+      body: '{}'
+    })
+    .then(function (response) {
+      // Get selectedMovie
+      media = JSON.parse(response);
+      return rp(options2)
+    })
+    .then(function (response) {
+      // Get latestMovies
+      relatedMovies = JSON.parse(response).results;
+      res.render('pages/media', {
+        activeLink: "home",
+        media: media,
+        mediaType: mediaType,
+        relatedMovies: relatedMovies
+      });
+    })
+    .catch(function (err) {
+      next(err)
+    });
 });
 
 module.exports = router;
