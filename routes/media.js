87f1251aa8a6b186a7dac9b2b2d26e69110c07ed
Eoghan <eoghankenny95@gmail.com>
2017-11-22 11:46:07 +0000

    added similar movies

--- routes/media.js
+++ routes/media.js
@@ -5,28 +5,14 @@ var https = require('https');
 var request = require("request");
 var rp = require('request-promise');
 
-var options2 = {
-  method: 'GET',
-  url: 'https://api.themoviedb.org/3/discover/movie',
-  qs: {
-    page: '1',
-    include_video: 'false',
-    include_adult: 'false',
-    sort_by: 'primary_release_date.desc',
-    language: 'en-US',
-    api_key: config.api_key
-  },
-  body: '{}'
-};
-
 router.get('/', function (req, res, next) {
   var id = req.query.id;
   var mediaType = req.query.media_type;
-  var media, relatedMovies;
+  var media, relatedMedia;
 
   rp({
       method: 'GET',
-      url: 'https://api.themoviedb.org/3/movie/' + id,
+      url: 'https://api.themoviedb.org/3/' + mediaType + '/' + id,
       qs: {
         api_key: config.api_key
       },
@@ -35,16 +21,24 @@ router.get('/', function (req, res, next) {
     .then(function (response) {
       // Get selectedMovie
       media = JSON.parse(response);
-      return rp(options2)
+      return rp({
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/' + mediaType + '/' + id + '/similar',
+        qs: {
+          api_key: config.api_key,
+          language: 'en-US',
+          page: '1'
+        },
+        body: '{}'
+      })
     })
     .then(function (response) {
       // Get latestMovies
-      relatedMovies = JSON.parse(response).results;
+      relatedMedia = JSON.parse(response).results;
       res.render('pages/media', {
-        activeLink: "home",
-        media: media,
         mediaType: mediaType,
-        relatedMovies: relatedMovies
+        media: media,
+        relatedMovies: relatedMedia
       });
     })
     .catch(function (err) {
