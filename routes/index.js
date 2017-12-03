91b2c9debf744a0befb01fe8cefcc2f0808dd625
Eoghan <eoghankenny95@gmail.com>
2017-12-03 21:03:49 +0000

    removed the latest media from index page

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,6 @@ var options = { method: 'GET',
   body: '{}' };
 
 var options2 = { method: 'GET',
-  url: 'https://api.themoviedb.org/3/discover/movie',
-  qs:
-   { page: '1',
-     include_video: 'false',
-     include_adult: 'false',
-     sort_by: 'primary_release_date.desc',
-     language: 'en-US',
-     api_key: config.api_key },
-  body: '{}' };
-
-var options3 = { method: 'GET',
   url: 'https://api.themoviedb.org/3/discover/tv',
   qs: {
     page: '1',
@@ -34,17 +23,6 @@ var options3 = { method: 'GET',
     language: 'en-US',
     api_key: config.api_key
   },
-  body: '{}' };
-
-var options4 = { method: 'GET',
-  url: 'https://api.themoviedb.org/3/discover/tv',
-  qs:
-   { page: '1',
-     include_video: 'false',
-     include_adult: 'false',
-     sort_by: 'primary_release_date.desc',
-     language: 'en-US',
-     api_key: config.api_key },
   body: '{}' };
 
 router.get('/', function(req, res, next) {
@@ -76,24 +54,6 @@ router.get('/', function(req, res, next) {
 
       return rp(options2)
     })
-    .then(function (response){
-      // Get latestMovies
-      latestMovies = JSON.parse(response).results;
-      if (req.user) {
-        latestMovies.forEach(function(movie) {
-          movie.isFavourite = false;
-          movie.isInWatchlist = false;
-          for (let i = 0; i < req.user.movies.length; i++) {
-            if (req.user.movies[i].id == movie.id ) {
-              movie.isFavourite = req.user.movies[i].isFavourite;
-              movie.isInWatchlist = req.user.movies[i].isInWatchlist;
-              break;
-            }
-          }
-        })
-      }
-      return rp(options3)
-    })
     .then(function (response) {
       // Get popularTvShows
       popularTvShows = JSON.parse(response).results;
@@ -113,33 +73,14 @@ router.get('/', function(req, res, next) {
           }
         })
       }
-      return rp(options4)
-    })
-    .then(function (response){
-      // Get latestTvShows
-      latestTvShows = JSON.parse(response).results;
-      if (req.user) {
-        latestTvShows.forEach(function(tvShow) {
-          tvShow.isFavourite = false;
-          tvShow.isInWatchlist = false;
-          for (let i = 0; i < req.user.tv_shows.length; i++) {
-            if (req.user.movies[i].id == tvShow.id ) {
-              tvShow.isFavourite = req.user.tv_shows[i].isFavourite;
-              tvShow.isInWatchlist = req.user.tv_shows[i].isInWatchlist;
-              break;
-            }
-          }
-        })
-      }
       res.render('pages/index', {
         activeLink: "home",
         isLoggedIn: !!req.user,
         username: req.user? req.user.username : null,
         popularMovies: popularMovies,
-        latestMovies: latestMovies,
-        popularTvShows: popularTvShows,
-        latestTvShows: latestTvShows
+        popularTvShows: popularTvShows
       });
+      return rp(options4)
     })
     .catch(function (err) {
       next(err)
