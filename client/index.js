d241d94a5937d87055da8f67f02f6a4cd54f1f3b
larfoley <lar_x87@yahoo.com>
2017-12-10 21:29:44 +0000

    new bundle

--- client/index.js
+++ client/index.js
@@ -2,5 +2,5 @@ import './styles/index.scss';
 // import ejsClient from './js/ejsClient.js';
 // import json from "./js/getJson.js";
 import './js/nav.js';
-import './js/form.js';
+// import './js/form.js';
 import "./js/media-search.js"
